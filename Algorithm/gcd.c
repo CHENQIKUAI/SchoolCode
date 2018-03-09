26336062db83241b3f1ce48762afdc9ea9dbbbbb
CHENQIKUAI <1779299572@qq.com>
2018-03-09 15:57:59 +0800

    modified

--- Algorithm/gcd.c
+++ Algorithm/gcd.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-long gcd(long m, long n) {
+int gcd(int m, int n) {
 	if (m == 0 && n == 0) {
-		printf("Input error, you can not input two 0.\n");
+		fprintf(stderr, "Input error, you can not input two 0.\n");
 		exit(-1);
 	}
-	long temp;
+	int temp;
 	while (n != 0) {
 		temp = n;
 		n = m % n;
@@ -14,14 +14,14 @@ long gcd(long m, long n) {
 	}
 	return m;
 }
-long gcd2(long m, long n) {
+int gcd2(int m, int n) {
 
 	if (m == 0 && n == 0) {
-		printf("Input error, you can not input two 0.\n");
+		fprintf(stderr,"Input error, you can not input two 0.\n");
 		exit(-1);
 	}
 
-	long k = m < n ? m : n;
+	int k = m < n ? m : n;
 	if (k == 0) {
 		return m > n ? m : n;
 	}
@@ -33,13 +33,13 @@ long gcd2(long m, long n) {
 		}
 	}
 }
-long gcd3(long m, long n) {
-	long r = 1;
+int gcd3(int m, int n) {
+	int r = 1;
 	if (m == 0 && n == 0) {
-		printf("Input error, you can not input two 0.\n");
+		fprintf(stderr,"Input error, you can not input two 0.\n");
 		exit(-1);
 	}
-	long k = m < n ? m : n;
+	int k = m < n ? m : n;
 	if (k == 0) {
 		return m > n ? m : n;
 	}
@@ -72,18 +72,12 @@ long gcd3(long m, long n) {
 	free(array);
 	return r;
 }
+
 int main(int argc, char const *argv[])
 {
-	long a, b;
+	int a, b;
 	printf("Please enter two positive integers!\n");
-	// scanf("%ld %ld", &a, &b);
-	// printf("The answer is: %ld\n", gcd(a, b));
-	int i = 0;
-	printf("For test! If there have one 0, then the Algorithm is wrong\n");
-	for (i = 0; i < 100; ++i) {
-		a = rand() % 100;
-		b = rand() % 100;
-		printf("%d ", gcd(a, b) == gcd3(a, b));
-	}
+	scanf("%d %d", &a, &b);
+	printf("The answer is: %d\n", gcd3(a, b));
 	return 0;
 }
